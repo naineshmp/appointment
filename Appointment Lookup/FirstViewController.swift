b913852ce41f74116e1b22a19e8ef0272439bb79
Nainesh Patel <nano@MacBook-Pro.local>
2017-12-01 21:40:56 -0800

    Initial Commit

---
+++ Appointment Lookup/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Appointment Lookup
+//
+//  Created by Nainesh Patel on 12/1/17.
+//  Copyright © 2017 Nainesh Patel. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
