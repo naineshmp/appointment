7ac8a06f518bc5592c92f1d82a9819d50ffb627a
Nainesh Patel <nano@MacBook-Pro.local>
2017-12-03 12:26:18 -0800

    one more comment

--- Appointment Lookup/SecondViewController.swift
+++ Appointment Lookup/SecondViewController.swift
@@ -13,6 +13,7 @@ class SecondViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("hello")
+        print("nainesh")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
