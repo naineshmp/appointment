9f46ac56f2f5feaa8c99d46892d061ce5b281f61
Nainesh Patel <nano@macbook-pro.local>
2017-12-13 18:37:02 -0800

    added appointmentcell.swft

---
+++ Appointment Lookup/Calendar/Cells/AppointmentCell.swift
@@ -0,0 +1,20 @@
+//
+//  AppointmentCell.swift
+//  Appointment Lookup
+//
+//  Created by Nainesh Patel on 12/13/17.
+//  Copyright © 2017 Nainesh Patel. All rights reserved.
+//
+//
+
+import UIKit
+
+class AppointmentCell: UITableViewCell {
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var noteLabel: UILabel!
+    
+}
+
+
