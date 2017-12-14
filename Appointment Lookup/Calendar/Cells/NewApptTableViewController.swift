5a6134bae944f578e0261ff9b25a26bbdaa7bd76
Nainesh Patel <nano@macbook-pro.local>
2017-12-13 18:17:47 -0800

    asdsadas

---
+++ Appointment Lookup/Calendar/Cells/NewApptTableViewController.swift
@@ -0,0 +1,325 @@
+//
+//  NewApptTableViewController.swift
+//  Appt
+//
+//  Created by Agustin Mendoza Romo on 5/17/17.
+//  Copyright © 2017 AgustinMendoza. All rights reserved.
+//
+
+import UIKit
+import CoreData
+import JTAppleCalendar
+
+class NewApptTableViewController: UITableViewController {
+    
+    
+ 
+    let formatter = DateFormatter()
+    
+  
+    
+    var calendarViewHidden = true
+    var appointmentScrolled = false
+    
+    // Calendar Color
+    let outsideMonthColor = UIColor.lightGray
+    let monthColor = UIColor.darkGray
+    let selectedMonthColor = UIColor.white
+    let currentDateSelectedViewColor = UIColor.black
+    
+    
+    
+    
+    
+    @IBOutlet var calendarView: JTAppleCalendarView!
+    @IBOutlet weak var timeSlotLabel: UILabel!
+    @IBOutlet weak var patientLabel: UILabel!
+    @IBOutlet weak var noteTextView: UITextView!
+    @IBOutlet weak var costTextField: UITextField!
+    @IBOutlet weak var dateDetailLabel: UILabel!
+    
+    @IBAction func cancelButton(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func confirmAppointment(_ sender: UIBarButtonItem) {
+      //  confirmAppointment()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupCalendarView()
+        
+       
+        
+        noLargeTitles()
+      
+        setupKeyboardNotification()
+        
+        calendarView.scrollToDate(Date(), animateScroll: false)
+        calendarView.selectDates( [Date()] )
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+       
+    }
+    
+    
+    
+    func noLargeTitles(){
+        if #available(iOS 11.0, *) {
+            navigationItem.largeTitleDisplayMode = .never
+            tableView.dragDelegate = self as? UITableViewDragDelegate
+        }
+    }
+    
+    func setupKeyboardNotification() {
+        NotificationCenter.default.addObserver(self, selector: #selector(NewApptTableViewController.keyboardWillShow(notification:)), name: NSNotification.Name.UIKeyboardDidShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(NewApptTableViewController.keyboardWillHide(notification:)), name: NSNotification.Name.UIKeyboardDidHide, object: nil)
+    }
+    
+    @objc func keyboardWillShow(notification: NSNotification) {
+        if let keyboardHeight = (notification.userInfo?[UIKeyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue.height {
+            tableView.contentInset = UIEdgeInsetsMake(0, 0, keyboardHeight + 20, 0)
+        }
+    }
+    
+    @objc func keyboardWillHide(notification: NSNotification) {
+        UIView.animate(withDuration: 0.2, animations: {
+            self.tableView.contentInset = UIEdgeInsetsMake(0, 0, 0, 0)
+        })
+    }
+    
+}
+
+extension NewApptTableViewController {
+    func toggleCalendarView() {
+        calendarViewHidden = !calendarViewHidden
+        
+        tableView.beginUpdates()
+        tableView.endUpdates()
+        appointmentScrolled = true
+    }
+    
+    func updateDateDetailLabel(date: Date){
+        formatter.dateFormat = "MMMM dd, yyyy"
+        dateDetailLabel.text = formatter.string(from: date)
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.section == 0 && indexPath.row == 0 {
+            toggleCalendarView()
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if calendarViewHidden && indexPath.section == 0 && indexPath.row == 1 {
+            return 0
+        } else {
+            return super.tableView(tableView, heightForRowAt: indexPath)
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = UIView()
+        headerView.backgroundColor = UIColor.white
+        return headerView
+    }
+    
+    override func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        let footerView = UIView()
+        footerView.backgroundColor = UIColor.white
+        return footerView
+    }
+    
+    //  override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+    //    if calendarViewHidden && section == 0 {
+    //      return 40
+    //    } else {
+    //      return 1
+    //    }
+    //  }
+}
+
+
+
+extension NewApptTableViewController {
+    func handleCellSelected(view: JTAppleCell?, cellState: CellState) {
+        guard let validCell = view as? CalendarDayCell else { return }
+        if validCell.isSelected {
+            validCell.selectedView.isHidden = false
+        } else {
+            validCell.selectedView.isHidden = true
+        }
+    }
+    
+    func handleCellTextColor(view: JTAppleCell?, cellState: CellState) {
+        guard let validCell = view as? CalendarDayCell else {
+            return
+        }
+        
+        if validCell.isSelected {
+            validCell.dateLabel.textColor = selectedMonthColor
+        } else {
+            if cellState.dateBelongsTo == .thisMonth {
+                validCell.dateLabel.textColor = monthColor
+            } else {
+                validCell.dateLabel.textColor = outsideMonthColor
+            }
+        }
+    }
+    
+    func setupCalendarView() {
+        // Setup Calendar Spacing
+        calendarView.minimumLineSpacing = 0
+        calendarView.minimumInteritemSpacing = 0
+        
+        // Setup Labels
+        calendarView.visibleDates{ (visibleDates) in
+            self.setupViewsFromCalendar(from: visibleDates)
+        }
+    }
+    
+    func setupViewsFromCalendar(from visibleDates: DateSegmentInfo ) {
+        guard let date = visibleDates.monthDates.first?.date else { return }
+        
+        formatter.dateFormat = "MMMM dd, yyyy"
+        dateDetailLabel.text = formatter.string(from: date)
+        
+        calendarView.selectDates( [date] )
+        
+        updateDateDetailLabel(date: date)
+   
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    }
+}
+
+
+extension NewApptTableViewController {
+    func loadAppointmentsForDate(date: Date){
+        
+    }
+    
+    func fullDayPredicate(for date: Date) -> NSPredicate {
+        var calendar = Calendar.current
+        calendar.timeZone = NSTimeZone.local
+        
+        let dateFrom = calendar.startOfDay(for: date)
+        var components = calendar.dateComponents([.year, .month, .day, .hour, .minute], from: dateFrom)
+        components.day! += 1
+        let dateTo = calendar.date(from: components)
+        let datePredicate = NSPredicate(format: "(%@ <= date) AND (date < %@)", argumentArray: [dateFrom, dateTo as Any])
+        
+        return datePredicate
+    }
+    
+    func fetchAppointmentsForDay() {
+     
+    }
+}
+
+extension NewApptTableViewController: JTAppleCalendarViewDataSource {
+    
+    func configureCalendar(_ calendar: JTAppleCalendarView) -> ConfigurationParameters {
+        formatter.dateFormat = "yyyy MM dd"
+        formatter.timeZone = Calendar.current.timeZone
+        formatter.locale = Calendar.current.locale
+        
+        var parameters: ConfigurationParameters
+        var startDate = Date()
+        var endDate = Date()
+        if let calendarStartDate = formatter.date(from: "2017 01 01"),
+            let calendarEndndDate = formatter.date(from: "2017 12 31") {
+            startDate = calendarStartDate
+            endDate = calendarEndndDate
+        }
+        parameters = ConfigurationParameters(startDate: startDate, endDate: endDate)
+        return parameters
+    }
+}
+
+
+extension NewApptTableViewController: JTAppleCalendarViewDelegate {
+    func calendar(_ calendar: JTAppleCalendarView, willDisplay cell: JTAppleCell, forItemAt date: Date, cellState: CellState, indexPath: IndexPath) {
+        
+    }
+    
+    // Display Cell
+    func calendar(_ calendar: JTAppleCalendarView, cellForItemAt date: Date, cellState: CellState, indexPath: IndexPath) -> JTAppleCell {
+        let cell = calendar.dequeueReusableJTAppleCell(withReuseIdentifier: "CalendarDayCell", for: indexPath) as! CalendarDayCell
+        cell.dateLabel.text = cellState.text
+        
+        handleCellSelected(view: cell, cellState: cellState)
+        handleCellTextColor(view: cell, cellState: cellState)
+        
+        return cell
+    }
+    
+    func calendar(_ calendar: JTAppleCalendarView, didSelectDate date: Date, cell: JTAppleCell?, cellState: CellState) {
+        handleCellSelected(view: cell, cellState: cellState)
+        handleCellTextColor(view: cell, cellState: cellState)
+        
+        updateDateDetailLabel(date: date)
+        loadAppointmentsForDate(date: date)
+        
+        //    calendarViewDateChanged()
+    }
+    
+    func calendar(_ calendar: JTAppleCalendarView, didDeselectDate date: Date, cell: JTAppleCell?, cellState: CellState) {
+        handleCellSelected(view: cell, cellState: cellState)
+        handleCellTextColor(view: cell, cellState: cellState)
+    }
+    
+    func calendar(_ calendar: JTAppleCalendarView, didScrollToDateSegmentWith visibleDates: DateSegmentInfo) {
+        if appointmentScrolled {
+            setupViewsFromCalendar(from: visibleDates)
+        }
+    }
+}
+
+extension NewApptTableViewController: NSFetchedResultsControllerDelegate {
+    
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.beginUpdates()
+    }
+    
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.endUpdates()
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        switch (type) {
+        case .insert:
+            if let indexPath = newIndexPath {
+                print("Appt Added")
+                tableView.insertRows(at: [indexPath], with: .fade)
+            }
+            break;
+        case .delete:
+            print("Appt Deleted")
+            if let indexPath = indexPath {
+                tableView.deleteRows(at: [indexPath], with: .fade)
+            }
+            break;
+        case .update:
+            if let indexPath = indexPath {
+                print("Appt Changed and updated")
+                tableView.reloadRows(at: [indexPath], with: .fade)
+            }
+        default:
+            print("...")
+        }
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange sectionInfo: NSFetchedResultsSectionInfo, atSectionIndex sectionIndex: Int, for type: NSFetchedResultsChangeType) {
+    }
+}
+
+
+
---
+++ Appointment Lookup/Calendar/Cells/TimeSlotsCVC.swift
@@ -0,0 +1,163 @@
+//
+//  TimeSlotsCVC.swift
+//  Appt
+//
+//  Created by Agustin Mendoza Romo on 8/8/17.
+//  Copyright © 2017 AgustinMendoza. All rights reserved.
+//
+
+import UIKit
+import FirebaseDatabase
+import FirebaseAuth
+
+class TimeSlotsCVC: UICollectionViewController {
+    
+    private let reuseIdentifier = "TimeSlotCell"
+    
+    
+    var timeSlotter = TimeSlotter()
+    var appointmentDate: Date!
+    var formatter = DateFormatter()
+    var timeSlots = [Date]()
+    let currentDate = Date()
+    var currentAppointments: [Appointment]?
+    
+    var TimeSlotList = Array<TimeSlot>()
+    var ref: DatabaseReference!
+    var keyString: String = "NULL"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(appointmentDate)
+        setupTimeSlotter()
+        navBarDropShadow()
+        ref = Database.database().reference()
+        let timeSlot = TimeSlot()
+        timeSlot.time = "8.00"
+        timeSlot.slot = 10
+        self.TimeSlotList.append(timeSlot)
+        print(self.keyString)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        
+        navBarNoShadow()
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        getKeyString()    }
+    
+    
+    
+    public func getKeyString(){
+        print("--started;;-")
+        ref.child("timeSlots").observeSingleEvent(of: .value, with: { (snapShot) in
+            if let snapDict = snapShot.value as? [String:AnyObject]{
+                for each in snapDict{
+                    let userEmail = each.value["user"] as! String
+                    if(userEmail == (Auth.auth().currentUser?.email)!)
+                    {
+                        self.keyString = each.key
+                        print("key selected")
+                        self.getAllTimeSlots()
+                        return
+                    }
+                }
+            }
+        })
+    }
+    
+    public func getAllTimeSlots(){
+        getKeyString()
+        self.TimeSlotList.removeAll()
+        print("started with keystring", self.keyString )
+        ref.child("timeSlots").child(self.keyString).child("12-21-2017").observeSingleEvent(of: .value, with: { (snapShot) in
+            if let snapDict = snapShot.value as? [String:AnyObject]{
+                for each in snapDict{
+                    let timeSlot = TimeSlot()
+                    timeSlot.time = each.key
+                    timeSlot.slot = each.value as! Int
+                    self.TimeSlotList.append(timeSlot)
+                    self.TimeSlotList.sort { $0.time.compare($1.time, options: .numeric) == .orderedAscending }
+                    
+                }
+                print("count","",self.TimeSlotList.count)
+                //self.collectionView?.reloadSections(IndexSet(integer : 0))
+            }
+        })
+        
+    }
+    
+    func setupTimeSlotter() {
+        timeSlotter.configureTimeSlotter(openTimeHour: 9, openTimeMinutes: 0, closeTimeHour: 17, closeTimeMinutes: 0, appointmentLength: 30, appointmentInterval: 15)
+        //    if let appointmentsArray = currentAppointments {
+        //     // timeSlotter.currentAppointments = appointmentsArray.map { $0.date }
+        //    }
+        //    guard let timeSlots = timeSlotter.getTimeSlotsforDate(date: appointmentDate) else {
+        //      print("There is no appointments")
+        //      return }
+        //
+        //    self.timeSlots = timeSlots
+    }
+    
+    func navBarDropShadow() {
+        self.navigationController?.navigationBar.layer.masksToBounds = false
+        self.navigationController?.navigationBar.layer.shadowColor = UIColor.black.cgColor
+        self.navigationController?.navigationBar.layer.shadowOpacity = 0.2
+        self.navigationController?.navigationBar.layer.shadowOffset = CGSize(width: 0, height: 4)
+        self.navigationController?.navigationBar.layer.shadowRadius = 4
+    }
+    
+    func navBarNoShadow(){
+        self.navigationController?.navigationBar.layer.masksToBounds = true
+        self.navigationController?.navigationBar.layer.shadowColor = .none
+        self.navigationController?.navigationBar.layer.shadowOpacity = 0
+        self.navigationController?.navigationBar.layer.shadowOffset = CGSize(width: 0, height: 0)
+        self.navigationController?.navigationBar.layer.shadowRadius = 0
+    }
+    
+    
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.TimeSlotList.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! TimeSlotCell
+        
+        if cell.isSelected {
+            cell.timeSlotView.backgroundColor = UIColor(red: 205, green: 141, blue: 254, alpha: 1)
+            cell.timeLabel.textColor = .white
+        }
+        
+        if(TimeSlotList.count>0)
+        {
+        //let timeSlot =/ timeSlots[indexPath.row]
+        formatter.dateFormat = "H:mm"
+        cell.timeLabel.text = "9.00"
+        }
+        return cell
+    }
+    
+    
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+        print("this one selected")
+        //    if let destinationVC = self.navigationController?.viewControllers[0] as?  NewApptTableViewController {
+        //      destinationVC.selectedTimeSlot = timeSlots[indexPath.row]
+        //      self.navigationController?.popViewController(animated: true)
+        //    } else if let destinationVC = self.navigationController?.viewControllers[0] as? UpdateApptTVC {
+        //      destinationVC.selectedTimeSlot = timeSlots[indexPath.row]
+        //      self.navigationController?.popViewController(animated: true)
+        //    }
+    }
+    
+}
+
+
+
+
