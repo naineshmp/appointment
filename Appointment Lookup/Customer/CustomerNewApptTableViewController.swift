6f598d7073df237827ba0b06378ef1f92c7aa7f7
Nainesh Patel <nano@macbook-pro.local>
2017-12-20 18:32:01 -0800

    REMVOED MENDOZA AND MAIN STORYBOARD..

--- Appointment Lookup/Customer/CustomerNewApptTableViewController.swift
+++ Appointment Lookup/Customer/CustomerNewApptTableViewController.swift
@@ -1,11 +1,3 @@
-//
-//  CustomerNewApptTableViewController.swift
-//  Appt
-//
-//  Created by Agustin Mendoza Romo on 5/17/17.
-//  Copyright © 2017 AgustinMendoza. All rights reserved.
-//
-
 import UIKit
 import CoreData
 import JTAppleCalendar
